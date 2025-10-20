7771b89e370730461d9401adcec52548e4c5a828
Matheus Maia <mathmferreira@hotmail.com>
2025-10-20 15:29:44 -0300

    refactor: remove unneeded methods on repositories

--- src/main/java/com/techmath/ecommerce/domain/repositories/ProductRepository.java
+++ src/main/java/com/techmath/ecommerce/domain/repositories/ProductRepository.java
@@ -4,12 +4,8 @@ import com.techmath.ecommerce.domain.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
-
-    List<Product> findByCategory(String category);
-
 }
--- src/main/java/com/techmath/ecommerce/infrastructure/search/repositories/ProductSearchRepository.java
+++ src/main/java/com/techmath/ecommerce/infrastructure/search/repositories/ProductSearchRepository.java
@@ -1,20 +1,9 @@
 package com.techmath.ecommerce.infrastructure.search.repositories;
 
 import com.techmath.ecommerce.infrastructure.search.documents.ProductDocument;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
-
 @Repository
 public interface ProductSearchRepository extends ElasticsearchRepository<ProductDocument, String> {
-
-    Page<ProductDocument> findByNameContaining(String name, Pageable pageable);
-
-    Page<ProductDocument> findByCategory(String category, Pageable pageable);
-
-    Page<ProductDocument> findByPriceBetween(BigDecimal minPrice, BigDecimal maxPrice, Pageable pageable);
-
 }
